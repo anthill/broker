1049af6a638f907a5a115226420eb75fe788ec0c
Alexandre Vallette <vallettea@gmail.com>
2015-01-22 11:30:30 +0100

    part of tunnel and arguments

--- brocker.js
+++ brocker.js
@@ -7,23 +7,52 @@ var fsm = require('./fsm.js');
 
 
 
-function processRequest(command) {
+function processRequest(rawCommand) {
 
-  switch(command) {
-      case "status":
-          return fsm.state;
-          break;
-      case "connect3G":
-          return fsm.connect3G();
-          break;
-      case "disconnect3G":
-          return fsm.disconnect3G();
-          break;
-      case "cleanConnexion":
-          return fsm.cleanConnexion();
-          break;
-      default:
-          return "Unrecognized command " + command
+  var commandArgs = rawCommand.split(":");
+
+  switch(commandArgs.length) {
+
+    case 0:
+      return "Unrecognized command " + rawCommand;
+      break;
+
+    case 1:
+      var command = commandArgs[0];
+
+      switch(command) {
+        case "status":
+            return fsm.state;
+            break;
+        case "connect3G":
+            return fsm.connect3G();
+            break;
+        case "disconnect3G":
+            return fsm.disconnect3G();
+            break;
+        case "cleanConnexion":
+            return fsm.cleanConnexion();
+            break;
+        case "cleanTunnel":
+            return fsm.cleanTunnel();
+            break;
+        default:
+            return "Unrecognized command " + command;
+            break;
+      }
+
+    case 2:
+
+      switch(commandArgs[0]) {
+        case "makeTunnel":
+            return fsm.makeTunnel(commandArgs[1]);
+            break;
+        default:
+            return "Unrecognized command " + rawCommand;
+            break;
+
+      }
+  
   }
 
 }
--- fsm.js
+++ fsm.js
@@ -6,11 +6,13 @@ var kill = require('tree-kill');
 var Promise = require('es6-promise').Promise;
 
 var CONNECTION_TIMOUT = 20 * 1000;
+var SSH_OK = 3 * 1000;
 
 
 var fsm = new machina.Fsm({
 
     wvdialPid: null,
+    sshPid: null,
 
     connect3G: function() {
 
@@ -18,8 +20,8 @@ var fsm = new machina.Fsm({
 
         if (this.state !== "3G_connected") {
 
-            var pp = new Promise(function(resolve, reject){
-                var myProcess = spawn("wvdial", ["3G"], {detached: true});
+            new Promise(function(resolve, reject){
+                var myProcess = spawn("wvdial", ["3G"]);
                 console.log("nodeprocess :", myProcess.pid, "myProcess: ", process.pid);
 
                 myProcess.stderr.on("data", function(chunkBuffer){
@@ -38,42 +40,107 @@ var fsm = new machina.Fsm({
                 });
 
 
-            });
-
-            pp.then(function(pid){
+            })
+            .then(function(pid){
                 self.wvdialPid = pid;
                 self.transition( "3G_connected" );
+                return "SUCCESS";
             })
             .catch(function(err){
-                console.log(err.msg, pp);
+                console.log(err.msg);
                 console.log("Could not connect. Cleanning...");
-                if (err.pid > 0){
-                    kill(err.pid, 'SIGKILL');
-                } else {
-                    console.log("could not kill signal whose pid is not an integer")
-                }
+                self.cleanConnexion(err.pid)
+                return "FAILURE";
             });
 
         } else {
             console.log("Already connected.")
+            return "SUCCESS";
         }
         
     },
 
-    cleanConnexion: function(){
-
+    cleanConnexion: function(pid){
+        // TODO this fucntion should lookup throught ps for wvdial and ppp
+        pid = pid || this.wvdialPid;
+        console.log("Cleaning pid ", pid);
+        var out;
+        if (pid > 0){
+            kill(pid, 'SIGKILL');
+            out = "SUCCESS";
+        } else {
+            console.log("could not kill signal whose pid is not an integer");
+            out = "FAILURE";
+        };
+        this.transition( "3G_disconnected" );
+        return ou;
     },
 
     disconnect3G: function() {
         console.log("killing process", this.wvdialPid);
+        var out;
         if (this.wvdialPid > 0){
             kill(this.wvdialPid, 'SIGKILL');
+            out = "SUCCESS";
         } else {
-            console.log("could not kill signal null")
-        }
+            console.log("could not kill signal null");
+            out = "FAILURE";
+        };
         this.transition( "3G_disconnected" );
+        return out;
     },
 
+
+    makeTunnel: function(port) {
+
+        if (this.state === "3G_disconnected") {
+
+        }
+
+        new Promise(function(resolve, reject){
+            var myProcess = spawn("ssh", ["-N", "-R", port + ":localhost:22", "ants"]);
+            console.log("nodeprocess :", myProcess.pid, "myProcess: ", process.pid);
+
+            myProcess.stderr.on("data", function(chunkBuffer){
+                var message = chunkBuffer.toString();
+                console.log("=> " + message);
+                if (message.indexOf("Warning: remote port forwarding failed for listen port") !== -1){
+                    reject({pid: myProcess.pid, msg:"[Brocker] Port already in use."});
+                } else {
+                    setTimeout(function(){resolve(myProcess.pid), SSH_OK);
+                }
+                
+            });
+
+
+        })
+        .then(function(pid){
+            self.sshPid = pid;
+            self.transition( "3G_tunnel" );
+            return "SUCCESS";
+        })
+        .catch(function(err){
+            console.log(err.msg);
+            console.log("Could not make the tunnel. Cleanning...");
+            // self.cleanConnexion(err.pid)
+            return "FAILURE";
+        });
+
+    },
+
+    cleanTunnel: function() {
+        console.log("killing process", this.sshPid);
+        var out;
+        if (this.sshPid > 0){
+            kill(this.sshPid, 'SIGKILL');
+            out = "SUCCESS";
+        } else {
+            console.log("could not kill signal null");
+            out = "FAILURE";
+        };
+        this.transition( "3G_connected" );
+        return out;
+    },
 
     initialState: "3G_disconnected",
 
@@ -91,6 +158,12 @@ var fsm = new machina.Fsm({
         "3G_disconnected" : {
             "tell.disconnected" : function( payload ) {
                 console.log("disconnected")
+            }
+        },
+
+        "3G_tunnel" : {
+            "tell.tunnel" : function( payload ) {
+                console.log("tunnel")
             }
         }
     }
