dd789a29f31d52bab3283ebf020b8b39842bfde6
4rzael <maxime.agor@epitech.eu>
2015-07-21 15:01:40 +0000

    web server patched, client added, TCP server patched

---
+++ app/client/client.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var client = require("./clientModule.js");
+var connectInfo = require("./connectInfo.js");
+
+// This is an example of the most basic TCP client possible using the clientModule.js
+
+client.start({address: connectInfo.address, port: connectInfo.port, name: connectInfo.name},
+	function(err, client) {
+		if (err) {
+			console.log("[ERROR] : " + err.message);
+		}
+	});
--- app/express/server.js
+++ app/express/server.js
@@ -9,55 +9,77 @@ var path = require('path');
 var server = http.createServer(app);
 var io = require('socket.io')(server);
 
-app.use("/socket.io.js", express.static(path.join(__dirname, './node_modules/socket.io/node_modules/socket.io-client/socket.io.js')));
-app.use("/dygraph-combined.js", express.static(path.join(__dirname, './node_modules/dygraphs/dygraph-combined.js')));
+var clients = {};
+
+app.use("/socket.io.js", express.static(path.join(__dirname, '../node_modules/socket.io/node_modules/socket.io-client/socket.io.js')));
+app.use("/dygraph-combined.js", express.static(path.join(__dirname, '../node_modules/dygraphs/dygraph-combined.js')));
 
 var serverPort = 7000;
 
-console.log("======", process.env.TCP_PORT_5000_TCP_ADDR, process.env.TCP_PORT_5000_TCP_PORT)
+console.log("======", process.env.TCP_PORT_5000_TCP_ADDR, process.env.TCP_PORT_5000_TCP_PORT, "======");
+
+// Get data from the TCP server
 
 var client = net.connect(
-	{
-		port: process.env.INTERNAL_PORT, 
-		host: process.env.TCP_PORT_5000_TCP_ADDR 
-	}, function() {
-		console.log("Connected");
-	}
-)
+{
+	port: process.env.INTERNAL_PORT, 
+	host: process.env.TCP_PORT_5000_TCP_ADDR 
+}, function() {
+	console.log("Connected");
+}
+);
+
+client.on('error', function(err) {
+	console.log("[ERROR] : " + err.message);
+	process.exit(-1);
+});
+
 
 
 function sendEveryInfos(socket) {
-    Object.keys(clients).forEach(function(key) {
+	console.log("sending every infos");
+	Object.keys(clients).forEach(function(key) {
 		if (clients[key] != undefined && clients[key].log != undefined && clients[key].log.length != 0) {
-		    socket.emit(
-		    	'data',
+			console.log("sending " + clients[key].name + " infos");
+			clients[key].log.forEach(function(log) {
+				console.log("sensor " + clients[key].name + " : " + JSON.stringify(log));
+				socket.emit('data',
 				{
 					cmd: "point",
-					name: getClientName(clients[key]),
-					x: clients[key].log[clients[key].log.length - 1].timestamp,
+					name: clients[key].name,
+					x: log.timestamp,
 					y: 0
-				}
-			);
+				});
+			});
 		}
-    });
+	});
 }
+
+
+// Send data to the frontend
 
 io.set('origins', '*:*');
 
 io.on('connection', function(socket) {
-    sendEveryInfos(socket);
-    client.on('data', function(data){
+
+	sendEveryInfos(socket); // Disable to only see new datas on the website
+	client.on('data', function(data){
+
 		var client = JSON.parse(data.toString());
+
+		clients[client.data.name] = client.data;
+		console.log("data received : " + JSON.stringify(client));
+
 		if (client.type === "connection") {
-			socket.emit('data', {cmd: "point", name: getClientName(client.data), x: client.dat.log[client.dat.log.length - 1].timestamp, y: 0});
-			socket.emit('data', {cmd: "point", name: getClientName(client.data), x: client.dat.log[client.dat.log.length - 1].timestamp, y: 1});
+			socket.emit('data', {cmd: "point", name: client.data.name, x: client.data.log[client.data.log.length - 1].timestamp, y: 0});
+			socket.emit('data', {cmd: "point", name: client.data.name, x: client.data.log[client.data.log.length - 1].timestamp, y: 1});
 		} else {
-			socket.emit('data', {cmd: "point", name: getClientName(client.data), x: client.dat.log[client.dat.log.length - 1].timestamp, y: 1});
-			socket.emit('data', {cmd: "point", name: getClientName(client.data), x: client.dat.log[client.dat.log.length - 1].timestamp, y: 0});
+			socket.emit('data', {cmd: "point", name: client.data.name, x: client.data.log[client.data.log.length - 1].timestamp, y: 1});
+			socket.emit('data', {cmd: "point", name: client.data.name, x: client.data.log[client.data.log.length - 1].timestamp, y: 0});
 		}
 	})
-
-    console.log("connection");
+	
+	console.log("web connection");
 });
 
 app.get('/', function (req, res) {
@@ -65,10 +87,10 @@ app.get('/', function (req, res) {
 });
 
 var server = server.listen(serverPort, function () {
-
-   var host = server.address().address
-   var port = server.address().port
-
-   console.log('Broker listening at http://%s:%s', host, port)
-
+	
+	var host = server.address().address
+	var port = server.address().port
+	
+	console.log('Broker listening at http://%s:%s', host, port)
+	
 });
