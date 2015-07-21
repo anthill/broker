d4c6c8429302c541b477ef0d9f79238e27377ecb
4rzael <maxime.agor@epitech.eu>
2015-07-21 15:12:14 +0000

    client patched (a problem made the connection to a distant TCP server impossible)

--- app/client/client.js
+++ app/client/client.js
@@ -5,7 +5,7 @@ var connectInfo = require("./connectInfo.js");
 
 // This is an example of the most basic TCP client possible using the clientModule.js
 
-client.start({address: connectInfo.address, port: connectInfo.port, name: connectInfo.name},
+client.start({host: connectInfo.host, port: connectInfo.port, name: connectInfo.name},
 	function(err, client) {
 		if (err) {
 			console.log("[ERROR] : " + err.message);
--- app/client/connectInfo.js
+++ app/client/connectInfo.js
@@ -1,6 +1,6 @@
 "use strict";
 
 module.exports = {
-	address: "tcp.ants.builders",	// TCP server address
+	host: "tcp.ants.builders",		// TCP server address
 	port: 5000,						// TCP server port
 	name: "Ant12"};					// Client name to use
